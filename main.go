f6acf6d8f3cbc0f0f9241bc9991898113d56731d
Munir Khakhi <munir@improwised.com>
2023-03-06 11:45:46 +0530

    One more add

--- main.go
+++ main.go
@@ -8,6 +8,11 @@ func main() {
 	total := Add(n1, n2)
 	fmt.Println(total)
 
+	var n5 int32 = 4
+	var n6 int32 = 4
+	sum := Add(n5, n6)
+	fmt.Println(sum)
+
 	var n3 float32 = 4.5
 	var n4 float32 = 5.5
 	totalFloat := Add(n3, n4)
